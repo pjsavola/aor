0db6f1ffbf32effd70509be841b73fb3f5b8108f
Petri Savola <petri.j.savola@gmail.com>
2023-08-05 18:57:55 +0300

    Clean up Board class a bit

--- src/aor/Board.java
+++ src/aor/Board.java
@@ -13,33 +13,21 @@ import java.util.*;
 public class Board extends JPanel {
 
     private final JFrame frame;
-    private final List<Line> lines = new ArrayList<>();
-    private final List<Node> nodes = new ArrayList<>();
     private final double scale;
     private final Dimension size;
     private final Image mapImage;
 
     private Point cursor;
 
-    private static final Set<Point> pointCache = new HashSet<>();
-
-    private Point p(int x, int y) {
-        final Point p = new Point(x, y);
-        if (pointCache.add(p)) {
-            return p;
-        }
-        return pointCache.stream().filter(a -> a.equals(p)).findAny().orElse(null);
-    }
-
     Board(JFrame frame) {
         this.frame = frame;
         final ImageIcon icon = new ImageIcon("map.jpg");
         final Rectangle rect = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
         scale = Math.min((double) rect.width / icon.getIconWidth(), (double) rect.height / icon.getIconHeight());
-        final int mapWidth = (int) (icon.getIconWidth() * scale);
-        final int mapHeight = (int) (icon.getIconHeight() * scale);
+        final int mapWidth = scale(icon.getIconWidth());
+        final int mapHeight = scale(icon.getIconHeight());
         size = new Dimension(mapWidth, mapHeight);
-        mapImage = getScaledInstance(toBufferedImage(icon.getImage()), mapWidth, mapHeight, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
+        mapImage = ImageUtil.getScaledInstance(ImageUtil.toBufferedImage(icon.getImage()), mapWidth, mapHeight, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true);
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -48,7 +36,7 @@ public class Board extends JPanel {
             public void mousePressed(MouseEvent e) {
                 final boolean rightClick = e.getButton() == MouseEvent.BUTTON3;
                 final Point point = e.getPoint();
-                for (Node node : nodes) {
+                for (Node node : Node.nodeMap.values()) {
                     if (node.contains(point)) {
                         System.err.println("Clicked " + node.getName());
                     }
@@ -97,7 +85,7 @@ public class Board extends JPanel {
                     nodeData.add(str);
                 } else {
                     final int[] s = Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
-                    final Line line = new Line(p(scale(s[0]), scale(s[1])), p(scale(s[2]), scale(s[3])), s[4] == 1);
+                    final Line line = new Line(new Point(scale(s[0]), scale(s[1])), new Point(scale(s[2]), scale(s[3])), s[4] == 1);
                     newLines.add(line);
                 }
             }
@@ -107,10 +95,6 @@ public class Board extends JPanel {
         for (int i = 0; i < newNodes.size(); ++i) {
             Node.initFromString(newNodes.get(i), nodeData.get(i), newLines, newNodes);
         }
-        lines.clear();
-        lines.addAll(newLines);
-        nodes.clear();
-        nodes.addAll(newNodes);
         repaint();
         System.err.println("Loaded from " + file.getAbsolutePath());
     }
@@ -129,7 +113,7 @@ public class Board extends JPanel {
         Set<Point> renderedPoints = new HashSet<>();
         g.setColor(Color.GREEN.darker());
         if (cursor != null) {
-            for (Node node : nodes) {
+            for (Node node : Node.nodeMap.values()) {
                 if (node.contains(cursor)) {
                     node.draw(g);
                 }
@@ -140,70 +124,5 @@ public class Board extends JPanel {
     @Override
     public Dimension getPreferredSize() {
         return size;
-    }
-
-    public static BufferedImage toBufferedImage(Image img)
-    {
-        if (img instanceof BufferedImage)
-        {
-            return (BufferedImage) img;
-        }
-
-        // Create a buffered image with transparency
-        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-
-        // Draw the image on to the buffered image
-        Graphics2D bGr = bimage.createGraphics();
-        bGr.drawImage(img, 0, 0, null);
-        bGr.dispose();
-
-        // Return the buffered image
-        return bimage;
-    }
-
-    public static BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, Object hint, boolean higherQuality)
-    {
-        int type = (img.getTransparency() == Transparency.OPAQUE) ?
-                BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
-        BufferedImage ret = (BufferedImage)img;
-        int w, h;
-        if (higherQuality) {
-            // Use multi-step technique: start with original size, then
-            // scale down in multiple passes with drawImage()
-            // until the target size is reached
-            w = img.getWidth();
-            h = img.getHeight();
-        } else {
-            // Use one-step technique: scale directly from original
-            // size to target size with a single drawImage() call
-            w = targetWidth;
-            h = targetHeight;
-        }
-
-        do {
-            if (higherQuality && w > targetWidth) {
-                w /= 2;
-                if (w < targetWidth) {
-                    w = targetWidth;
-                }
-            }
-
-            if (higherQuality && h > targetHeight) {
-                h /= 2;
-                if (h < targetHeight) {
-                    h = targetHeight;
-                }
-            }
-
-            BufferedImage tmp = new BufferedImage(w, h, type);
-            Graphics2D g2 = tmp.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
-            g2.drawImage(ret, 0, 0, w, h, null);
-            g2.dispose();
-
-            ret = tmp;
-        } while (w != targetWidth || h != targetHeight);
-
-        return ret;
     }
 }
---
+++ src/aor/ImageUtil.java
@@ -0,0 +1,71 @@
+package aor;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public abstract class ImageUtil {
+    public static BufferedImage toBufferedImage(Image img)
+    {
+        if (img instanceof BufferedImage)
+        {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+        // Draw the image on to the buffered image
+        Graphics2D bGr = bimage.createGraphics();
+        bGr.drawImage(img, 0, 0, null);
+        bGr.dispose();
+
+        // Return the buffered image
+        return bimage;
+    }
+
+    public static BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight, Object hint, boolean higherQuality)
+    {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ?
+                BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage)img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
+    }
+}
